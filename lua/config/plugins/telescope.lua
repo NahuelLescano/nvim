58d05fd7cad1d57864ab8aa93f271db4b9271f60
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-03-21 16:47:41 -0300

    Search hidden files and new keymap

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -11,6 +11,9 @@ return {
       pickers = {
         find_files = {
           theme = "ivy",
+          find_files = {
+            hidden = true
+          }
         },
         buffers = {
           theme = "dropdown",
@@ -23,6 +26,9 @@ return {
     telescope.load_extension("fzf")
     telescope.load_extension("noice")
 
+    local x = 5
+    print(x)
+
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fn', function()
@@ -31,6 +37,7 @@ return {
       })
     end, { desc = 'Telescope find nvim files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope grep string' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
