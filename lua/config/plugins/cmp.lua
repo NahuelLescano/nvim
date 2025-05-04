3ec89a84f840dc3d51087b803001d02a56088f8b
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-05-04 18:54:50 -0300

    Delete unnecessary table

--- lua/config/plugins/cmp.lua
+++ lua/config/plugins/cmp.lua
@@ -1,30 +1,28 @@
 return {
-  {
-    'saghen/blink.cmp',
-    dependencies = {
-      'rafamadriz/friendly-snippets',
-      'folke/lazydev.nvim'
+  'saghen/blink.cmp',
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    'folke/lazydev.nvim'
+  },
+  version = '*',
+  opts = {
+    keymap = { preset = 'default' },
+
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'mono'
     },
-    version = '*',
-    opts = {
-      keymap = { preset = 'default' },
 
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = 'mono'
-      },
-
-      sources = {
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            score_offset = 100,
-          },
+    sources = {
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
         },
       },
-
-      signature = { enabled = true }
     },
-  }
+
+    signature = { enabled = true }
+  },
 }
