92ed3878969d965dff31c98eeffc63ab2b13dccd
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-05-04 19:01:06 -0300

    Using hl instead of hightlight because of deprecated issues

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yaking (copying) text",
   group = vim.api.nvim_create_augroup("Yanking-highlight", { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
-
