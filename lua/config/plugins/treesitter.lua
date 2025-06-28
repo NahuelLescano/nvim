d059f4384ebbffcd6efa1699c2d6fafe4d59b023
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-06-28 19:13:00 -0300

    I was learning angular, but I'll never use it again

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require 'nvim-treesitter.configs'.setup {
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "json" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "json", "angular" },
       sync_install = false,
       auto_install = true,
       highlight = {
