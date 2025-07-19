245f3c86df0b9d4c58b6e7b750e7a2ffe16cbc83
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-19 14:35:00 -0300

    Deleting extra function

--- init.lua
+++ init.lua
@@ -2,10 +2,3 @@ require("options")
 require("keymaps")
 require("config.lazy")
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yaking (copying) text",
-  group = vim.api.nvim_create_augroup("Yanking-highlight", { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
-})
