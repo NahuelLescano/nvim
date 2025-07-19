5848eb96460bcf84fd5fcaec24fbbb366fac1ea7
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-19 15:38:37 -0300

    I blame you

---
+++ lua/config/plugins/git-blame-you.lua
@@ -0,0 +1,8 @@
+return {
+  "FabijanZulj/blame.nvim",
+  lazy = false,
+  config = function()
+    require('blame').setup {}
+    vim.keymap.set('n', '<leader>gb', '<cmd>BlameToggle<CR>', { desc = 'Toggle Git Blame' })
+  end,
+}
