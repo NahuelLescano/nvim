48e3562d5a7ec8e639917f437c28dbf3e9dfa851
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-19 14:33:14 -0300

    Better notify

--- lua/config/plugins/noice.lua
+++ lua/config/plugins/noice.lua
@@ -8,6 +8,10 @@ return {
   config = function()
     require("notify").setup({
       background_colour = "#000000",
+      merge_duplicates = true,
+      stages = "fade",
+      timeout = 1000,
+      render = "compact",
     })
 
     require("noice").setup({
@@ -18,8 +22,9 @@ return {
           ["cmp.entry.get_documentation"] = true,
         },
       },
+
       presets = {
-        bottom_search = true,         -- use a classic bottom cmdline for search
+        bottom_search = false,        -- use a classic bottom cmdline for search
         command_palette = true,       -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false,           -- enables an input dialog for inc-rename.nvim
