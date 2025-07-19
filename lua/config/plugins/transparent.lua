62fc6e6098a5fdc975e7477047e1dd2e4751d500
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-19 14:34:23 -0300

    Which key description

--- lua/config/plugins/transparent.lua
+++ lua/config/plugins/transparent.lua
@@ -9,17 +9,13 @@ return {
         'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
         'EndOfBuffer',
       },
-      -- table: additional groups that should be cleared
-      extra_groups = {},
-      -- table: groups you don't want to clear
       exclude_groups = {
         'NotifyBackground', 'NotifyINFO', 'NotifyERROR', 'NotifyWARN',
         'NotifyDEBUG', 'NotifyTRACE',
       },
-      -- function: code to be executed after highlight groups are cleared
-      -- Also the user event "TransparentClear" will be triggered
-      on_clear = function() end,
     })
-    vim.keymap.set("n", "<leader>tt", ":TransparentToggle<CR>")
+    vim.keymap.set("n", "<leader>tt", "<cmd>TransparentToggle<CR>", {
+      desc = "Toggle Transparent Background"
+    })
   end
 }
