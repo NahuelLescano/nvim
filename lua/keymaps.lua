c384c4ea4315e0313b0dd7faa0c54162916c48af
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-19 14:34:44 -0300

    All keymaps on the same level

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,6 +6,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" }
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>", { desc = "Next quickfix item" })
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>", { desc = "Previous quickfix item" })
 vim.keymap.set("n", "<M-q>", "<cmd>cclose<CR>", { desc = "Close quickfix window" })
+vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice notifications" })
+vim.keymap.set("n", "<leader>cc", "<cmd>CopilotChat<CR>", { desc = "Open Copilot Chat" })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yaking (copying) text",
@@ -21,9 +24,3 @@ vim.keymap.set("n", "<space>st", function ()
     vim.cmd.wincmd("J")
     vim.api.nvim_win_set_height(0, 15)
 end, { desc = "Open terminal in split" })
-
-vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
-vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice notifications" })
-
-vim.keymap.set("n", "<leader>cc", "<cmd>CopilotChat<CR>", { desc = "Open Copilot Chat" })
