74c91c979b2fde516fefe53718bdfdeb7d277b82
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-08-02 15:59:27 -0300

    Installing formatting and linting with nonels

---
+++ lua/config/plugins/nonels.lua
@@ -0,0 +1,22 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black.with({
+          extra_args = { "--fast" },
+        }),
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.biome,
+        require("none-ls.diagnostics.eslint_d"),
+      },
+    })
+  end
+}
