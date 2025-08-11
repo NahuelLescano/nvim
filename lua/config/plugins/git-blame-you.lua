ccfcbefb5194e1fd99d0f2547e07a40d1b027d8b
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-08-11 20:24:45 -0300

    Disable missing fiedls

--- lua/config/plugins/git-blame-you.lua
+++ lua/config/plugins/git-blame-you.lua
@@ -1,8 +1,9 @@
 return {
-	"FabijanZulj/blame.nvim",
-	lazy = false,
-	config = function()
-		require("blame").setup({})
-		vim.keymap.set("n", "<leader>gb", "<cmd>BlameToggle<CR>", { desc = "Toggle Git Blame" })
-	end,
+  "FabijanZulj/blame.nvim",
+  lazy = false,
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require("blame").setup({})
+    vim.keymap.set("n", "<leader>gb", "<cmd>BlameToggle<CR>", { desc = "Toggle Git Blame" })
+  end,
 }
