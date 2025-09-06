005c116f172a74d664ccd9b59a72a1a522587d15
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-09-06 13:27:27 -0300

    change some colors

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,32 +1,36 @@
 ---@diagnostic disable: missing-fields, undefined-field
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-    vim.cmd [[hi @tag.astro guifg=darkpink]]
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    vim.cmd([[hi @tag.astro guifg=darkpink]])
+    vim.cmd([[hi @tag.xml guifg=pink]])
 
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"markdown",
-				"json",
-			},
-			sync_install = true,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				disable = function(_, buf)
-					local max_filesize = 100 * 1024
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-				additional_vim_regex_highlighting = true,
-			},
-		})
-	end,
+    vim.cmd([[hi @tag.builtin.javascript guifg=pink]])
+    vim.cmd([[hi @tag.javascript guifg=pink]])
+
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "json",
+      },
+      sync_install = true,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+        additional_vim_regex_highlighting = true,
+      },
+    })
+  end,
 }
