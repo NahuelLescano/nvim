9761ce53fed0025f427f2eb4e9d4423888b2805c
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-09-30 21:58:54 -0300

    change tab to ctrl-j to accept suggestions

--- lua/config/plugins/copilot.lua
+++ lua/config/plugins/copilot.lua
@@ -1,3 +1,17 @@
 return {
 	"github/copilot.vim",
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+		vim.g.copilot_filetypes = {
+			["*"] = true,
+			["markdown"] = false,
+			["json"] = false,
+			["yaml"] = false,
+			["toml"] = false,
+			["help"] = false,
+			["TelescopePrompt"] = false,
+			["NvimTree"] = false,
+		}
+	end,
 }
