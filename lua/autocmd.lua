25797732761edc8ac13346159e11632e5836a8e5
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-10-26 18:02:06 -0300

    dont use autocmds for now

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,32 +1,33 @@
-vim.api.nvim_create_autocmd("BufWritePost", {
-	callback = function()
-		local ns = vim.api.nvim_create_namespace("save_highlight")
-
-		local line_nr = vim.api.nvim_win_get_cursor(0)[1] - 1
-		local line_text = vim.api.nvim_buf_get_lines(0, line_nr, line_nr + 1, false)[1]
-
-		if not line_text or line_text == "" then
-			return
-		end
-
-		local start_col = line_text:find("%S") - 1
-		local end_col = #line_text
-
-		local id = vim.api.nvim_buf_set_extmark(0, ns, line_nr, start_col, {
-			end_col = end_col,
-			hl_group = "Visual",
-		})
-
-		vim.defer_fn(function()
-			vim.api.nvim_buf_del_extmark(0, ns, id)
-		end, 300)
-	end,
-})
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yaking (copying) text",
-	group = vim.api.nvim_create_augroup("Yanking-highlight", { clear = true }),
-	callback = function()
-		vim.hl.on_yank()
-	end,
-})
+-- Examples of autocommands in Neovim using Lua API
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+-- 	callback = function()
+-- 		local ns = vim.api.nvim_create_namespace("save_highlight")
+--
+-- 		local line_nr = vim.api.nvim_win_get_cursor(0)[1] - 1
+-- 		local line_text = vim.api.nvim_buf_get_lines(0, line_nr, line_nr + 1, false)[1]
+--
+-- 		if not line_text or line_text == "" then
+-- 			return
+-- 		end
+--
+-- 		local start_col = line_text:find("%S") - 1
+-- 		local end_col = #line_text
+--
+-- 		local id = vim.api.nvim_buf_set_extmark(0, ns, line_nr, start_col, {
+-- 			end_col = end_col,
+-- 			hl_group = "Visual",
+-- 		})
+--
+-- 		vim.defer_fn(function()
+-- 			vim.api.nvim_buf_del_extmark(0, ns, id)
+-- 		end, 300)
+-- 	end,
+-- })
+--
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+-- 	desc = "Highlight when yaking (copying) text",
+-- 	group = vim.api.nvim_create_augroup("Yanking-highlight", { clear = true }),
+-- 	callback = function()
+-- 		vim.hl.on_yank()
+-- 	end,
+-- })
