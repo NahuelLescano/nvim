476b296945082ef2cd1bb1e6b0f729297e815697
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-12-09 23:59:11 -0300

    fix bug: I have to use `require(...)`

--- lua/config/plugins/nonels.lua
+++ lua/config/plugins/nonels.lua
@@ -1,38 +1,38 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.black.with({
-					extra_args = { "--fast" },
-				}),
-				null_ls.builtins.formatting.biome,
-				null_ls.builtins.formatting.eslint.with({
-					command = "bunx",
-					args = { "eslint", "--fix", "--stdin", "--stdin-filename", "$FILENAME", "--config", "eslint.config.js" },
-				}),
-				null_ls.builtins.diagnostics.eslint.with({
-					command = "bunx",
-					args = {
-						"eslint",
-						"--format",
-						"unix",
-						"--stdin",
-						"--stdin-filename",
-						"$FILENAME",
-						"--config",
-						"eslint.config.js",
-					},
-				}),
-				null_ls.builtins.formatting.shfmt,
-			},
-		})
-	end,
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black.with({
+          extra_args = { "--fast" },
+        }),
+        null_ls.builtins.formatting.biome,
+        require("none-ls.formatting.eslint").with({
+          command = "bunx",
+          args = { "eslint", "--fix", "--stdin", "--stdin-filename", "$FILENAME", "--config", "eslint.config.js" },
+        }),
+        require("none-ls.formatting.eslint").with({
+          command = "bunx",
+          args = {
+            "eslint",
+            "--format",
+            "unix",
+            "--stdin",
+            "--stdin-filename",
+            "$FILENAME",
+            "--config",
+            "eslint.config.js",
+          },
+        }),
+        null_ls.builtins.formatting.shfmt,
+      },
+    })
+  end,
 }
