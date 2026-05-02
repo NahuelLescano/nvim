375418b0ad3403821bcc35130513f76750e32f51
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-05-02 00:00:21 -0300

    feat: add new keymap to search a word under the cursor

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,38 +1,41 @@
-vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Source current file" })
-vim.keymap.set("n", "<leader>x", "<cmd>:lua .<CR>", { desc = "Run current file" })
-vim.keymap.set("v", "<leader>x", "<cmd>:lua .<CR>", { desc = "Run selected code" })
+local set = vim.keymap.set
 
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" })
-vim.keymap.set("n", "<M-j>", ":m .+1<CR>>==", { desc = "Move current line down" })
-vim.keymap.set("n", "<M-k>", ":m .-2<CR>>==", { desc = "Move current line up" })
+set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Source current file" })
+set("n", "<leader>x", "<cmd>:lua .<CR>", { desc = "Run current file" })
+set("v", "<leader>x", "<cmd>:lua .<CR>", { desc = "Run selected code" })
 
-vim.keymap.set("n", "<leader>j", "<cmd>cnext<CR>", { desc = "Next quickfix item" })
-vim.keymap.set("n", "<leader>k", "<cmd>cprev<CR>", { desc = "Previous quickfix item" })
-vim.keymap.set("n", "<leader>q", "<cmd>cclose<CR>", { desc = "Close quickfix window" })
+set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" })
+set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" })
+set("n", "<M-j>", ":m .+1<CR>>==", { desc = "Move current line down" })
+set("n", "<M-k>", ":m .-2<CR>>==", { desc = "Move current line up" })
 
-vim.keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "Next buffer" })
-vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
-vim.keymap.set("n", "<leader>bd", "<cmd>%bd|e#<CR>", { desc = "Close all buffers except current" })
+set("n", "<leader>j", "<cmd>cnext<CR>", { desc = "Next quickfix item" })
+set("n", "<leader>k", "<cmd>cprev<CR>", { desc = "Previous quickfix item" })
+set("n", "<leader>q", "<cmd>cclose<CR>", { desc = "Close quickfix window" })
 
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
+set("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "Next buffer" })
+set("n", "<leader>bp", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+set("n", "<leader>bd", "<cmd>%bd|e#<CR>", { desc = "Close all buffers except current" })
 
-vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 
-vim.keymap.set("n", "<leader>tx", "<cmd>!chmod +x %<CR>", { silent = true }, { desc = "Make current file executable" })
+set("t", "<esc><esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
-vim.keymap.set("n", "<space>st", function()
+set("n", "<leader>tx", "<cmd>!chmod +x %<CR>", { silent = true }, { desc = "Make current file executable" })
+
+set("n", "<leader>st", function()
 	vim.cmd.vnew()
 	vim.cmd.term()
 	vim.cmd.wincmd("J")
 	vim.api.nvim_win_set_height(0, 10)
 end, { desc = "Open terminal in split" })
 
-vim.keymap.set("n", "<leader>tt", "<cmd>TransparentToggle<CR>", {
+set("n", "<leader>tt", "<cmd>TransparentToggle<CR>", {
 	desc = "Toggle Transparent Background",
 })
 
-vim.keymap.set("n", "<space>ft", function()
+set("n", "<leader>ft", function()
 	vim.lsp.buf.format()
 end, { desc = "Format buffer with LSP" })
 
+set("n", "<leader>/", "*", { desc = "Search word under cursor" })
